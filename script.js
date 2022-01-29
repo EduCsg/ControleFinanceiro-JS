9a471b96cc04208a470734ef30509b443ce8a459
EduCsg <eduardocasagrande71@gmail.com>
2022-01-29 15:15:52 -0300

    Criou a parte lógica sem aplicação no html

---
+++ script.js
@@ -0,0 +1,52 @@
+const transactionsUl = document.querySelector("#transactions");
+
+const dummyTransactions = [
+	{ id: 1, name: "Bolo de Brigadeiro", amount: -20 },
+	{ id: 2, name: "Salário", amount: 300 },
+	{ id: 3, name: "Torta de Frango", amount: -10 },
+	{ id: 4, name: "Violão", amount: 150 },
+];
+
+const addTransactionIntoDOM = (transaction) => {
+	const operator = transaction.amount < 0 ? "-" : "+";
+	const CSSClass = transaction.amount < 0 ? "minus" : "plus"; //decide se o texto armazenado será 'minus' ou 'plus'
+	const amountWithoutOperator = Math.abs(transaction.amount);
+	const li = document.createElement("li"); //cria o elemento <li>
+
+	li.classList.add(CSSClass); //define a class do <li> a partir do if/else da CSSClass
+	li.innerHTML = `
+	${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span>
+	 <button class="delete-btn">x</button>
+	`;
+
+	transactionsUl.prepend(li);
+};
+
+const updateBalanceValues = () => {
+	const transactionsAmounts = dummyTransactions.map(
+		(transaction) => transaction.amount,
+	);
+
+	const total = transactionsAmounts
+		.reduce((accumulator, transaction) => accumulator + transaction, 0)
+		.toFixed(2);
+
+	const income = transactionsAmounts
+		.filter((value) => value > 0)
+		.reduce((accumulator, value) => accumulator + value, 0)
+		.toFixed(2);
+
+	const expense = transactionsAmounts
+		.filter((value) => value < 0)
+		.reduce((accumulator, value) => accumulator + value)
+		.toFixed(2);
+
+	console.log(expense);
+};
+
+const init = () => {
+	dummyTransactions.forEach(addTransactionIntoDOM);
+	updateBalanceValues();
+};
+
+init();
